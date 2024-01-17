c7bd1842c600d29b235796a5da383ac8710f24d3
federicoverbicaro <federico.verbicaro@icloud.com>
2024-01-17 11:51:28 +0100

    feat:correzione style griglia

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+const buttonHtml = document.getElementById("bottone")
+const grigliHtml = document.getElementById("griglia")
+let casellaHtml = document.getElementById("casella")
+
+
+
+buttonHtml.addEventListener('click',function(){
+    grigliHtml.classList.remove("deactivate")
+    grigliHtml.classList.add("active")
+
+         
+})
+
+
+for(let i = 1; i <= 100; i++){
+
+    let casellaHtml =  document.createElement("div")
+    casellaHtml.classList = ("cubo") 
+    casellaHtml.innerHTML = `<span>${i}</span>`
+
+    casellaHtml.addEventListener('click',function(){
+        this.classList.toggle("coloreUnico")
+        console.log("hai cliccato sulla casella" + " " + this.innerText)
+    })
+
+
+    grigliHtml.appendChild(casellaHtml)
+
+ }   
+ 
+
